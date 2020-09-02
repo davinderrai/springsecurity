e4a9be9bcc0ab85e65ca1f8396fda219539cb882
Davinder Singh <simurgrai@gmail.com>
2020-09-02 09:10:29 +0530

    jwt app

---
+++ springjwt/src/main/java/com/example/demo/SecurityConfigurer.java
@@ -0,0 +1,6 @@
+package com.example.demo;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+}
